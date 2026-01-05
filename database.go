7fd5d9c914595e3cb5476b0942fbc7c8f9dc1aea
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2026-01-05 20:18:56 +0530

    Implemented

---
+++ database.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func connectDB() (*sql.DB, error) {
+	// Open connection
+	dsn := "root:1234@tcp(127.0.0.1:3306)/url_shortener"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Configure connection pool
+	db.SetMaxOpenConns(24)
+	db.SetMaxIdleConns(18)
+	db.SetConnMaxLifetime(1 * time.Minute)
+
+	// Test connection
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS main(
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		url TEXT NOT NULL UNIQUE,
+    	clicks INT DEFAULT 0,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)
+	`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Database initialized successfully")
+	return db, nil
+}
+
+// create record
+func addUrl(url string, db *sql.DB) (int64, error) {
+	result, err := db.Exec("INSERT INTO main (url) VALUES ?", url)
+	if err != nil {
+		return 0, err
+	}
+
+	// Get auto generated id
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+// fetch record
+func getUrl(id int64, db *sql.DB) (string, error) {
+	var url string
+	err := db.QueryRow("SELECT url FROM main WHERE id = ?", id).Scan(url)
+	if err == sql.ErrNoRows {
+		return "", fmt.Errorf("No record with id %d not found", id)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	// Increment clicks
+	db.Exec("UPDATE main SET clicks = clicks + 1 WHERE id = ?", id)
+
+	return url, nil
+}
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+func main() {
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error reading body: %v", err)
+			return
+		}
+		defer r.Body.Close()
+
+		urlStr := string(body)
+
+		// Validate body as url
+		_, err = url.ParseRequestURI(urlStr)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error parsing URL: %v", err)
+			return
+		}
+
+		// Store url in database and return id
+		id, err := addUrl(urlStr, db)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error generating url: %v", err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", strconv.FormatInt(id, 10))
+	})
+
+	http.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		idInt64, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Bad parameter id: %v", err)
+			return
+		}
+
+		// Fetch url from database by id
+		url, err := getUrl(idInt64, db)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error fetching url: %v", err)
+			return
+		}
+
+		// Return url
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", url)
+	})
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
